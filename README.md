36b99005d8637a724122aaa4792f27078958fa4e
Žan Fridau <zaanfr@gmail.com>
2024-09-22 00:12:47 +0200

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# My Personal Dotfiles
+
+These are my personal dotfiles for setting up WezTerm with Oh-My-Tmux.
+
+## Prerequisites
+
+Before installing WezTerm, you should install **Oh-My-Tmux**. You can do that by following the instructions in the [Oh-My-Tmux repository](https://github.com/gpakosz/.tmux).
+
+## WezTerm Configuration
+
+To use my WezTerm configuration, place the config file in the following directory:
+
+```bash
+~/.config/wezterm/
+```
---
+++ wezterm.lua
@@ -0,0 +1,30 @@
+local wezterm = require("wezterm")
+
+return {
+	-- Disable the tab bar completely
+	enable_tab_bar = false,
+
+	window_background_opacity = 0.93,
+
+	-- Set font (optional, adjust to your preference)
+	font = wezterm.font("CaskaydiaCove Nerd Font"), -- You can change this to your preferred font
+	font_size = 14.0, -- Adjust the font size here (e.g., 14.0, 16.0, etc.)
+
+	-- Allow window decorations for resizing
+	-- Set to "RESIZE" to keep it minimal while allowing resizing
+	window_decorations = "RESIZE",
+
+	-- Allow resizing when changing the font size
+	adjust_window_size_when_changing_font_size = true,
+
+	-- Disable WezTerm's fancy tab bar
+	use_fancy_tab_bar = false,
+
+	-- Set tmux with correct Homebrew path
+	-- Check your actual tmux path with `which tmux`
+	default_prog = { "/opt/homebrew/bin/tmux", "new-session", "-A", "-s", "default" },
+
+	-- Set the initial window size (optional)
+	initial_rows = 30,
+	initial_cols = 80,
+}
